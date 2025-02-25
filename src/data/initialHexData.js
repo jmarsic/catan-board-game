0b344c45ff40359b383f35e164ce190dc93e78e3
jmarsic <jmarsovac@gmail.com>
2025-02-25 22:38:03 +0100

    add HexDetails constant and update initialData array

--- src/data/initialHexData.js
+++ src/data/initialHexData.js
@@ -1,16 +1,27 @@
-import { FieldNumbers, FieldTypes } from "../constants/fieldConstants.js";
+import {
+  FieldNumbers,
+  FieldTypes,
+  HexDetails,
+} from "../constants/fieldConstants.js";
 import { shuffleArray } from "../utils/helpers.js";
 
 const shuffledNumbers = shuffleArray([...FieldNumbers]);
 const shuffledTypes = shuffleArray([...FieldTypes]);
 
 // shuffeled data without robber
-const shuffeledData = shuffledNumbers.map((number, index) => ({
+const shuffledData = shuffledNumbers.map((number, index) => ({
   number: number,
   color: shuffledTypes[index],
 }));
 
 // add robber {7, 'beige'}
-shuffeledData.push({ number: 7, color: "beige" });
+shuffledData.push({ number: 7, color: "beige" });
 
-export const initialData = shuffleArray([...shuffeledData]);
+const shuffledTiles = shuffleArray([...shuffledData]);
+
+export const initialData = shuffledTiles.map((tile, index) => ({
+  ...tile,
+  id: HexDetails[index].id,
+  vertices: HexDetails[index].vertices,
+  edges: HexDetails[index].edges,
+}));
