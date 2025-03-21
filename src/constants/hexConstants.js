de537ef99d745a47eb876e90675942f9a3a322ac
jmarsic <jmarsovac@gmail.com>
2025-03-21 21:12:23 +0100

    add row and column offsets and update vertices position

--- src/constants/hexConstants.js
+++ src/constants/hexConstants.js
@@ -3,13 +3,26 @@ export const HexHeight = 56;
 export const HexGap = 4;
 export const HexOffsetY = 28;
 
-export const VertexOffsets = [
-  [-50, -28], //top-left
-  [0, -28], // top-middle
-  [50, -28], // top-right
-  [-50, 28], // bottom-left
-  [0, 28], // bottom-middle
-  [50, 28], // bottom-right
+export const VerticesTopPositions = [
+  [0, 0],
+  [50, -28.87],
+  [102, 0],
+  [152, -28.87],
+  [204, 0],
+  [254, -28.87],
+  [306, 0],
+  [356, -28.87],
+  [408, 0],
+  [458, -28.87],
+  [510, 0],
 ];
 
+export const VerticesBottomPositions = [];
+
 export const RowLayout = [3, 4, 5, 4, 3];
+
+export const RowVerticesNumber = [7, 9, 11, 11, 9, 7];
+
+export const VerticesRowOffsets = [94, 42, -10, -10, 46, 92];
+
+export const VerticesColumnOffsets = [-10, 82.87, 169.74, 225.74, 314.61, 480.35];
